3133950c48e058bad8075b4ec545d719603d75f9
AtalayOkun <atalay.okun@gmail.com>
2025-12-08 20:51:20 +0300

    Initial Commit

---
+++ Pocket Flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket Flow
+//
+//  Created by Atalay Okun on 8.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pocket Flow/Pocket_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_FlowApp.swift
+//  Pocket Flow
+//
+//  Created by Atalay Okun on 8.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
